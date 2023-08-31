884c91f4b8740e2ba06732d4500fb0a3b1c79faf
Sandra Hoffberg <sandra@codeocean.com>
2023-08-31 14:40:52 +0000

    Edited prep_fastas.sh

--- code/prep_fastas.sh
+++ code/prep_fastas.sh
@@ -3,6 +3,3 @@ set -ex
 
 
 python3 /opt/CombFold/scripts/prepare_fastas.py ../data/subunits.json --stage pairs --output-fasta-folder ../results/fasta_pairs --max-af-size 1800
-
-
-python3 /opt/CombFold/scripts/prepare_fastas.py ../data/subunits.json --stage groups --output-fasta-folder ../results/fasta_groups --max-af-size 1800 --input-pairs-results ../results/fasta_pairs
