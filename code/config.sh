98a4e2b3f734151819314b360d542073ab1db471
Sandra Hoffberg <sandra@codeocean.com>
2023-09-28 15:42:47 +0000

    Edited config.sh

--- code/config.sh
+++ code/config.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
 if [ $# -eq 0 ]; then
-  echo "No arguments supplied"
+    echo "No arguments supplied"
 else
-  echo "args: $*"
+    echo "args:"
+    for i in $*; do 
+        echo $i 
+    done
+    echo ""
 fi
+
 
 if [ -z ${1} ]; then
     json_file=$(find -L ../data -name "*.json")
 else
     json_file=${1}
 fi
+
 
 if [ -z ${2} ]; then
     max_af_size=1800
