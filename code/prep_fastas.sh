547dd2a07e689b21c8666f29ebe7f40a19063651
Stephen Pirpinias <stephen@codeocean.com>
2023-10-02 18:04:55 +0000

    Edited prep_fastas.sh

--- code/prep_fastas.sh
+++ code/prep_fastas.sh
@@ -3,4 +3,9 @@ set -ex
 
 source ./config.sh
 
-python3 /opt/CombFold/scripts/prepare_fastas.py ${json_file} --stage pairs --output-fasta-folder ../results/fasta_pairs --max-af-size ${max_af_size}
+if [ $json_count -eq 1 ];
+then
+    python3 /opt/CombFold/scripts/prepare_fastas.py ${json_file} --stage pairs --output-fasta-folder ../results/fasta_pairs --max-af-size ${max_af_size}
+
+else
+    echo "This Capsule only accepts 1 json file at a time."
